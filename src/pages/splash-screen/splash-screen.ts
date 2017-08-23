34b52e3a599c00059d793e97a0c648b93635f0c4
Andy Schwartz <email": null>
2017-08-23 15:15:43 -0500

    add image to splash screen

---
+++ src/pages/splash-screen/splash-screen.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@IonicPage()
+@Component({
+  selector: 'page-splash-screen',
+  templateUrl: 'splash-screen.html',
+})
+export class SplashScreenPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public splashScreen: SplashScreen) {
+  }
+
+  
+}
